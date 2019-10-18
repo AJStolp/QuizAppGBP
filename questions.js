126864caa336e89bc6c6bc2a6039092ed266e439
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-10-18 08:28:27 -0500

    first commit

---
+++ questions.js
@@ -0,0 +1,72 @@
+const QUESTIONS = [
+    {
+        //Question 1
+        text: 'Who did the Packers play in the first Super Bowl?',
+        answers: [
+            { text: 'Oakland', correct: false },
+            { text: 'Patriots', correct: false },
+            { text: 'Kanas City', correct: true },
+            { text: 'Da Bears', correct: false }
+        ]
+    },
+
+    {
+        //Question 2
+        text: 'Who is the current Quarterback?',
+        answer: [
+            {text: 'Mason Crosby', correct: false},
+            {text: 'Brett Favre', correct: false},
+            {text: 'Aaron Rodgers', correct: true},
+            {text: 'Mike Daniels', correct: false}
+        ]
+    },
+    {
+        //Question 3
+        text: 'How many championships have the Green Bay Packers won?',
+        answer: [
+            {text: '10', correct: false},
+            {text: '6', correct: false},
+            {text: '8', correct: false},
+            {text: '13', correct: true}
+        ]
+    },
+    {
+        //Question 4
+        text: 'What team did they beat in their last Super Bowl appearance?',
+        answer: [
+            {text: 'Pittsburgh', correct: true},
+            {text: 'New England', correct: false,},
+            {text: 'Seattle', correct: false},
+            {text: 'kanas City', correct: false}
+        ]
+    },
+    {
+        //Question 5
+        text: 'What year was Lambeau Field built?',
+        answer: [
+            {text: '1950', correct: false},
+            {text: '1957', correct: true},
+            {text: '1960', correct: false},
+            {text: '2000', correct: false}
+        ]
+    },
+    {
+        //Question 6
+        text: 'Since what year have season tickets been sold out?',
+        answer: [
+            {text: '2010', correct: false},
+            {text: '1998', correct: false},
+            {text: '1989', correct: false},
+            {text: '1960', correct: true}
+        ]
+    },
+    {
+        //Question 7
+        text: 'Who was the Packers first head coach?',
+        answer: [
+            {text: 'Bart Star', correct: false},
+            {text: 'Curly Lambeau', correct: true},
+            {text: 'matt Lefleur', correct: false},
+            {text: 'Vince Lombardi', correct: false}
+        ]
+    }];
