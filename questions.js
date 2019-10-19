5fbeff6c3dc8ee25731066233de4d15234fc6be0
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-10-19 11:03:32 -0500

    added quiestion to other js file now only one js file

--- questions.js
+++ questions.js
@@ -3,11 +3,12 @@ const QUESTIONS = [
         //Question 1
         question: 'Who did the Packers play in the first Super Bowl?',
         answers: [
-            { text: 'Oakland', correct: false },
-            { text: 'Patriots', correct: false },
-            { text: 'Kanas City', correct: true },
-            { text: 'Da Bears', correct: false }
+            { text: 'Oakland'},
+            { text: 'Patriots'},
+            { text: 'Kanas City'},
+            { text: 'Da Bears'}
         ]
+        correctAnswer: 'Kanas City'
     },
 
     {
@@ -70,5 +71,41 @@ const QUESTIONS = [
             {text: 'Vince Lombardi', correct: false}
         ]
     }];
+        //Start of js function for Quiz App-------------------------------------------------------------------------
+    function startQuiz() {
+        $('.jsStart').on('click', function(event){
+            updateQuestion();
+        })
+        //this function will listen for a click on the start button
+        //then it will take users to the first question
+    }
+    function updateQuestion(QUESTIONS) {
+        let question = [];
+        for(let i = 0; i < QUESTIONS.length; i++){
+            
+        }
+        //this function updates the updates questions after the next button is clicked
+    }
+    function checkAnswer(){
+        //this checks to see if answer is correct
+    }
+    function nextQuestion() {
+        //this function listens for a click on the touchdown? button if the answer is correct
+        //the user is taken to the next question
+    }
+    function UpdateScore(){
+        //this function will see if the answer is correct then update
+        //the score accordingly
+    }
+    function restartQuiz() {
+        //this function takes user to the beginning
+    }
+    function callAllFunctions() {
+         startQuiz();
+         updateQuestion();
+         checkAnswer();
+         nextQuestion();
+         restartQuiz();
+    }
    
    
