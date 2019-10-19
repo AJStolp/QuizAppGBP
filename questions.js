2616b184d38703094fb2d3b03db928801ff040cf
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-10-19 14:01:09 -0500

    updates questions and answers

--- questions.js
+++ questions.js
@@ -7,7 +7,7 @@ const QUESTIONS = [
             { text: 'Patriots'},
             { text: 'Kanas City'},
             { text: 'Da Bears'}
-        ]
+        ],
         correctAnswer: 'Kanas City'
     },
 
@@ -15,61 +15,67 @@ const QUESTIONS = [
         //Question 2
         question: 'Who is the current Quarterback?',
         answer: [
-            {text: 'Mason Crosby', correct: false},
-            {text: 'Brett Favre', correct: false},
-            {text: 'Aaron Rodgers', correct: true},
-            {text: 'Mike Daniels', correct: false}
-        ]
+            {text: 'Mason Crosby'},
+            {text: 'Brett Favre'},
+            {text: 'Aaron Rodgers'},
+            {text: 'Mike Daniels',}
+        ],
+        correctAnswer: 'Aaron Rodgers'
     },
     {
         //Question 3
         question: 'How many championships have the Green Bay Packers won?',
         answer: [
-            {text: '10', correct: false},
-            {text: '6', correct: false},
-            {text: '8', correct: false},
-            {text: '13', correct: true}
-        ]
+            {text: '10'},
+            {text: '6'},
+            {text: '8',},
+            {text: '13'}
+        ],
+        correctAnswer: '13'
     },
     {
         //Question 4
         question: 'What team did they beat in their last Super Bowl appearance?',
         answer: [
-            {text: 'Pittsburgh', correct: true},
-            {text: 'New England', correct: false,},
-            {text: 'Seattle', correct: false},
-            {text: 'kanas City', correct: false}
-        ]
+            {text: 'Pittsburgh'},
+            {text: 'New England'},
+            {text: 'Seattle'},
+            {text: 'kanas City'}
+        ],
+        correctAnswer: 'Pittsburgh'
     },
     {
         //Question 5
         question: 'What year was Lambeau Field built?',
         answer: [
-            {text: '1950', correct: false},
-            {text: '1957', correct: true},
-            {text: '1960', correct: false},
-            {text: '2000', correct: false}
-        ]
+            {text: '1950'},
+            {text: '1957'},
+            {text: '1960'},
+            {text: '2000'}
+        ],
+        correctAnswer: '1957'
     },
     {
         //Question 6
         question: 'Since what year have season tickets been sold out?',
         answer: [
-            {text: '2010', correct: false},
-            {text: '1998', correct: false},
-            {text: '1989', correct: false},
-            {text: '1960', correct: true}
-        ]
+            {text: '2010'},
+            {text: '1998'},
+            {text: '1989'},
+            {text: '1960'}
+        ],
+        correctAnswer: '1960'
     },
     {
         //Question 7
         question: 'Who was the Packers first head coach?',
         answer: [
-            {text: 'Bart Star', correct: false},
-            {text: 'Curly Lambeau', correct: true},
-            {text: 'matt Lefleur', correct: false},
-            {text: 'Vince Lombardi', correct: false}
-        ]
+            {text: 'Bart Star'},
+            {text: 'Curly Lambeau'},
+            {text: 'matt Lefleur'},
+            {text: 'Vince Lombardi'}
+        ],
+        correctAnswer: 'Curly Lambeau'
     }];
         //Start of js function for Quiz App-------------------------------------------------------------------------
     function startQuiz() {
