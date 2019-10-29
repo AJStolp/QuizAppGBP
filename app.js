39dbb3951f0f3149f4807660904307c7e1f0f569
Anthony Stolp <anthonystolp@anthonys-mbp-2.home>
2019-10-29 17:05:23 -0500

    refactor

---
+++ app.js
@@ -0,0 +1,126 @@
+const STATES = {
+    START: 'start',
+    QUESTION: 'question',
+    CORRECT: 'correct',
+    INCORRECT: 'incorrect',
+    END: 'end'
+}
+
+//Declare Variables
+let currentState, numCorrect, numIncorrect, currentQuestionIndex
+
+// set start state
+function loadStart() {
+    // set state 
+    currentState = STATES.START
+
+    // reset counters
+    numCorrect = 0
+    numIncorrect = 0
+    currentQuestionIndex = 0
+
+    // set id on main element to state
+    $('main').attr('id', currentState)
+
+    // update inner HTML in <main>
+    $('main').html(`
+    <article class="panel">
+        <section class="inner">
+            <h1>FOOTBALL QUIZ</h1>
+            <button>Let's Go!</button>
+        </section>
+    </article>`)
+    console.log('first part is working')
+}
+
+function loadNextQuestion() {
+    currentState = STATES.QUESTION
+    $('main').attr('id', currentState)
+    $('form').removeClass()
+    $('button').html(`Submit`)
+
+    let questionsHTML = createQuestionsHTML()
+
+    // update inner HTML in <main>
+    $('main').html(`
+    <article class="panel">
+        <section class="inner">
+            <h1>${currentQuestionIndex + 1}. ${QUESTIONS[currentQuestionIndex].question}</h1>
+            <form id="quiz">
+                ${questionsHTML}
+                <button type="submit">Submit</button>
+                <div id='incorrect'></div>
+            </form>
+        </section>
+        <footer>${currentQuestionIndex + 1} of 7</footer>
+    </article>`)
+
+    updateCorrectIncorrect()
+    console.log('second part is working');
+}
+
+function loadEnd(){
+    currentState = STATE.END
+    $('main').attr('id', currentState)
+    $('button').html(`Re-take!`)
+
+    let message = 
+        numCorrect / QUESTIONS.length >= 0.7
+            ? 'Nice Job'
+            : 'Better Luck Next Year!'
+
+    //update inner html
+    $('main').html(`
+    <article class="panel">
+        <section class="inner">
+            <p>YOU GOT<br /><span class="large">${numCorrect} / ${QUESTIONS.length}</span></p>
+            <p>${message}</p>
+            <button>RE-TAKE</button>
+        </section>
+    </article>`)
+
+    //remove counter from incorrect and correct
+    $('footer.footer').remove()
+}
+
+function createQuestionsHTML(){
+    return QUESTIONS[currentQuestionIndex].answers
+        .map((question, index) =>{
+            return `
+                <div class='answer'>
+                    <input type='radio' name='answer' value='${index}' id='option${index}' class=''/>
+                    <label for='option${index}'>${question.text}</label>
+                </div>`
+        })
+        .join('\n')
+}
+
+//checkAnswersValid 
+
+function checkAnswersValid(){
+    let values = $('input[name="answer"]:checked').val();
+            if(values == `${QUESTIONS[currentQuestionIndex].answers}`){
+                $('#incorrect').text('The Crowd Go\'s Wild!');
+            }else{
+                $('#incorrect').text('Incomplete Pass!')
+    }
+}
+//updateForm
+
+//updateCorrectIncorrect
+
+//loadButtonListener 
+
+function loadButtonListener(){
+    $('.inner').on('click', 'button', function(e){
+        e.preventDefault();
+        console.log('working?!')
+        loadStart();
+        //loadNextQuestion()
+    })
+}
+
+$(() => {
+    //loadButtonListener()
+    loadStart()
+});
--- questions.js
+++ questions.js
@@ -1,163 +1,230 @@
 const QUESTIONS = [
     {
-        //Question 1
         question: 'Who did the Packers play in the first Super Bowl?',
-            
-            'choice1': 'Oakland',
-            'choice2': 'Patriots',
-            'choice3': 'Kansas City',
-            'choice4': 'Da Bears',
-    
-        correctAnswer: 'Kansas City'
+        answers: [
+            { text: 'Oakland', correct: false },
+            { text: 'Patriots', correct: false },
+            { text: 'Kansas City', correct: true },
+            { text: 'Da Bears', correct: false }
+        ]
     },
-
     {
-        //Question 2
         question: 'Who is the current Quarterback?',
-    
-        'choice1': 'Mason Crosby',
-        'choice2': 'Brett Favre',
-        'choice3': 'Aaron Rodgers',
-        'choice4': 'Mike Daniels',
-        
-        correctAnswer: 'Aaron Rodgers'
+        answers: [
+            { text: 'Mason Crosby', correct: false },
+            { text: 'Brett Favre', correct: false },
+            { text: 'Aaron Rodgers', correct: true },
+            { text: 'Mike Daniels', correct: false }
+        ]
     },
     {
-        //Question 3
-        question: 'How many championships have the Green Bay Packers won?',
-        
-            'choice1': '10',
-            'choice2': '6',
-            'choice3': '8',
-            'choice4': '13',
-    
-        correctAnswer: '13'
+        question: 'How many Championships have the GreenBay Packers won?',
+        answers: [
+            { text: 10, correct: false },
+            { text: 6, correct: false },
+            { text: 8, correct: false },
+            { text: 13, correct: true }
+        ]
     },
     {
-        //Question 4
         question: 'What team did they beat in their last Super Bowl appearance?',
-        
-        'choice1': 'Pittsburgh',
-        'choice2': 'New England',
-        'choice3': 'Seattle',
-        'choice4': 'Kanas City',
-        
-        correctAnswer: 'Pittsburgh'
+        answers: [
+            { text: 'Pittsburgh', correct: true },
+            { text: 'New England', correct: false },
+            { text: 'Seattle', correct: false },
+            { text: 'Kansas City', correct: false }
+        ]
     },
     {
-        //Question 5
         question: 'What year was Lambeau Field built?',
-        
-        'choice1': '1950',
-        'choice2': '1957',
-        'choice3': '1960',
-        'choice4': '2000',
-        
-        correctAnswer: '1957'
+        answers: [
+            { text: 1950, correct: false },
+            { text: 1957, correct: true },
+            { text: 1960, correct: false },
+            { text: 2000, correct: false }
+        ]
     },
     {
-        //Question 6
         question: 'Since what year have season tickets been sold out?',
-        
-        'choice1': '2010',
-        'choice2': '1998',
-        'choice3': '1989',
-        'choice4': '1960',
-        
-        correctAnswer: '1960'
+        answers: [
+            { text: 2010, correct: false },
+            { text: 1998, correct: false },
+            { text: 1989, correct: false },
+            { text: 1960, correct: true }
+        ]
     },
     {
-        //Question 7
-        question: 'Who was the Packers first head coach?',
-        
-        'choice1': 'Bart Star',
-        'choice2': 'Curly Lambeau',
-        'choice3': 'Matt Lefleur',
-        'choice4': 'Vince Lombardi',
-        
-        correctAnswer: 'Curly Lambeau'
-    }];
-        //Start of js functions for Quiz App-------------------------------------------------------------------------
-    //Variables to update score and question number    
-    let score = 0;
-    let questionNumber = 0;
-    function startQuiz() {
-        
-        $('.start').on('click', function(){
-            updateQuestion();
-            $('.headline').hide();
-            nextQuestion();
-            $(this).html('<button class="nxt">Next</button>');
-            //UpdateScore();
-            $(this).on('click', function(e){
-                e.preventDefault;
-                $('.formz').children().empty();
-                console.log('working working working');
-            });
-        })
-        //this function will listen for a click on the start button
-        //then it will take users to the first question
-    }
-    function updateQuestion() {
-       const quizQuestions = `<section class='question'>
-                <form class='formz'>
-                <h4>${questionNumber}<span>/7 - Question Number</span></h4>
-                <h4>${score}<span>/49 - Score</span></h4>
-                <h2>${QUESTIONS[questionNumber].question}</h2>
-                <label>
-                <input type='radio' id='r1' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice1}'>${QUESTIONS[questionNumber].choice1}
-                </label>
-                <label>
-                <input type='radio' id='r2' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice2}'>${QUESTIONS[questionNumber].choice2}
-                </label>
-                <label>
-                <input type='radio' id='r3' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice3}'>${QUESTIONS[questionNumber].choice3}
-                </label>
-                <label>
-                <input type='radio' id='r4' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice4}'>${QUESTIONS[questionNumber].choice4}
-                </label>
-                <section class='tdScoresForDays'>
-                <div id='incorrect'><div>
-                </section>
-                <button type='submit' id='td'>Touchdown?</button>
-                </form>
-       </section>`
-       $('.jsList').on('click').append(quizQuestions);
-       $('.jsList').on('click', '#td', function(e){
-           e.preventDefault();
-           checkAnswer()
-           $(this).hide();
-       })
-    }      
-    function checkAnswer(){
-            let values = $('input[name="forRadio1"]:checked').val();
-            if(values == `${QUESTIONS[questionNumber -1].correctAnswer}`){
-                $('#incorrect').text('The Crowd Go\'s Wild!');
-                UpdateScore(); 
-            }else{
-                $('#incorrect').text('Incomplete Pass!')
-        }
-    }
+        question: 'Who is the Packers first head coach?',
+        answers: [
+            { text: 'Bart Star', correct: false },
+            { text: 'Curly Lambeau', correct: true },
+            { text: 'Matt Lefleur', correct: false },
+            { text: 'Vince Lombardi', correct: false }
+        ]
+    },
+]
 
 
-    function nextQuestion() {
-       `${questionNumber ++}`;
-        
-        //this function listens for a click on the next button if the answer is correct
-        //the user is taken to the next question
-    }
+// const QUESTIONS = [
+//     {
+//         //Question 1
+//         question: 'Who did the Packers play in the first Super Bowl?',
+            
+//             'choice1': 'Oakland',
+//             'choice2': 'Patriots',
+//             'choice3': 'Kansas City',
+//             'choice4': 'Da Bears',
+    
+//         correctAnswer: 'Kansas City'
+//     },
 
-    function UpdateScore(){
-        let tdScore = `${score += 7}`;
+//     {
+//         //Question 2
+//         question: 'Who is the current Quarterback?',
+    
+//         'choice1': 'Mason Crosby',
+//         'choice2': 'Brett Favre',
+//         'choice3': 'Aaron Rodgers',
+//         'choice4': 'Mike Daniels',
         
-        console.log(tdScore);
+//         correctAnswer: 'Aaron Rodgers'
+//     },
+//     {
+//         //Question 3
+//         question: 'How many championships have the Green Bay Packers won?',
         
-        //this function will see if the answer is correct then update
-        //the score accordingly
-    }
+//             'choice1': '10',
+//             'choice2': '6',
+//             'choice3': '8',
+//             'choice4': '13',
+    
+//         correctAnswer: '13'
+//     },
+//     {
+//         //Question 4
+//         question: 'What team did they beat in their last Super Bowl appearance?',
+        
+//         'choice1': 'Pittsburgh',
+//         'choice2': 'New England',
+//         'choice3': 'Seattle',
+//         'choice4': 'Kanas City',
+        
+//         correctAnswer: 'Pittsburgh'
+//     },
+//     {
+//         //Question 5
+//         question: 'What year was Lambeau Field built?',
+        
+//         'choice1': '1950',
+//         'choice2': '1957',
+//         'choice3': '1960',
+//         'choice4': '2000',
+        
+//         correctAnswer: '1957'
+//     },
+//     {
+//         //Question 6
+//         question: 'Since what year have season tickets been sold out?',
+        
+//         'choice1': '2010',
+//         'choice2': '1998',
+//         'choice3': '1989',
+//         'choice4': '1960',
+        
+//         correctAnswer: '1960'
+//     },
+//     {
+//         //Question 7
+//         question: 'Who was the Packers first head coach?',
+        
+//         'choice1': 'Bart Star',
+//         'choice2': 'Curly Lambeau',
+//         'choice3': 'Matt Lefleur',
+//         'choice4': 'Vince Lombardi',
+        
+//         correctAnswer: 'Curly Lambeau'
+//     }];
+//         //Start of js functions for Quiz App-------------------------------------------------------------------------
+//     //Variables to update score and question number    
+//     let score = 0;
+//     let questionNumber = 0;
+//     function startQuiz() {
+        
+//         $('.start').on('click', function(){
+//             updateQuestion();
+//             $('.headline').hide();
+//             nextQuestion();
+//             $(this).html('<button class="nxt">Next</button>');
+//             //UpdateScore(); 
+//             /*$(this).on('click', function(e){
+//                 e.preventDefault;
+//                 $('.formz').children().empty();
+//                 console.log('working working working')*/;
+//             });
+//         }
+//         //this function will listen for a click on the start button
+//         //then it will take users to the first question
+    
+//     function updateQuestion() {
+//        const quizQuestions = `<section class='question'>
+//                 <form class='formz'>
+//                 <h4>${questionNumber}<span>/7 - Question Number</span></h4>
+//                 <h4>${score}<span>/49 - Score</span></h4>
+//                 <h2>${QUESTIONS[questionNumber].question}</h2>
+//                 <label>
+//                 <input type='radio' id='r1' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice1}'>${QUESTIONS[questionNumber].choice1}
+//                 </label>
+//                 <label>
+//                 <input type='radio' id='r2' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice2}'>${QUESTIONS[questionNumber].choice2}
+//                 </label>
+//                 <label>
+//                 <input type='radio' id='r3' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice3}'>${QUESTIONS[questionNumber].choice3}
+//                 </label>
+//                 <label>
+//                 <input type='radio' id='r4' name='forRadio1' class='rdo' value='${QUESTIONS[questionNumber].choice4}'>${QUESTIONS[questionNumber].choice4}
+//                 </label>
+//                 <section class='tdScoresForDays'>
+//                 <div id='incorrect'><div>
+//                 </section>
+//                 <button type='submit' id='td'>Touchdown?</button>
+//                 </form>
+//        </section>`
+//        $('.jsList').on('click').append(quizQuestions);
+//        $('.jsList').on('click', '#td', function(e){
+//            e.preventDefault();
+//            checkAnswer()
+//            $(this).hide();
+//        })
+//     }      
+//     function checkAnswer(){
+//             let values = $('input[name="forRadio1"]:checked').val();
+//             if(values == `${QUESTIONS[questionNumber -1].correctAnswer}`){
+//                 $('#incorrect').text('The Crowd Go\'s Wild!');
+//                 UpdateScore(); 
+//             }else{
+//                 $('#incorrect').text('Incomplete Pass!')
+//         }
+//     }
 
-    function restartQuiz() {
-        //this function takes user to the beginning
-    }
 
-    $(startQuiz);
+//     function nextQuestion() {
+//        `${questionNumber ++}`;
+        
+//         //this function listens for a click on the next button if the answer is correct
+//         //the user is taken to the next question
+//     }
+
+//     function UpdateScore(){
+//         let tdScore = `${score += 7}`;
+        
+//         console.log(tdScore);
+        
+//         //this function will see if the answer is correct then update
+//         //the score accordingly
+//     }
+
+//     function restartQuiz() {
+//         //this function takes user to the beginning
+//     }
+
+//     $(startQuiz);
